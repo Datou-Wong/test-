d0ad4aac64f9651b66ada3a9ab5c7f05fb3ffa24
Datou-Wong <tou0327@gmail.com>
2025-11-06 15:42:50 +0800

    首次上傳

---
+++ t.js
@@ -0,0 +1,28 @@
+const bmiData = {
+  records: [
+    {
+      status: "過輕",
+      color: "blue",
+    },
+    {
+      status: "正常",
+      color: "red",
+    },
+    {
+      status: "過重",
+      color: "orange",
+    },
+    {
+      status: "輕度肥胖",
+      color: "yellow",
+    },
+    {
+      status: "中度肥胖",
+      color: "black",
+    },
+    {
+      status: "重度肥胖",
+      color: "green",
+    },
+  ],
+};
